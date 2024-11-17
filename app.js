67741948660505f9f57a6d5da3132fb4f9cca8a0
KeyaanVegdani <130519545+KeyaanVegdani@users.noreply.github.com>
2024-11-17 00:06:41 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.dire.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
+
+fetch("experts.json")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      const title = card.querySelector("[data-title]")
+      const email = card.querySelector("[data-email]")
+      const img = card.querySelector("[data-img]")
+      const dir = card.querySelector("[data-directorate]")
+      const exp = card.querySelector("[data-expertise]")
+      const supervisor = card.querySelector("[data-supervisor]")
+      var nam = user.first + " " + user.last;
+      header.textContent = nam
+      body.textContent = user.biography
+      title.textContent = user.title
+      email.href = user.email
+      img.src = user.photo
+      dir.textContent = " ∎ " + user.directorate
+      supervisor.textContent = "Supervisor:" + " " + user.supervisor
+      exp.textContent = " ∎ " + user.expertise
+
+      userCardContainer.append(card)
+      return { name: nam, dire: user.directorate, element: card }
+    })
+  })
+
+
+function setDirectorate(input){
+    const value = input.toLowerCase();
+    users.forEach(user => {
+        const isVisible =
+            user.dire.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+    console.log(value);
+}
+
+function setAllVisible(){
+    users.forEach(user => {
+        const isVisible = true;
+        user.element.classList.toggle("hide", !isVisible)
+    })
+    console.log(value);
+}
